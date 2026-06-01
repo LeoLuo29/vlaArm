035faa87a5c19f80f577257778289c6b4c451206
LeoLuo29 <xiangbin.luo@outlook.com>
2026-06-01 14:49:07 -0700

    RDX drivers

--- drivers/RDX3225.py
+++ drivers/RDX3225.py
@@ -1,4 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
+# PWM: 50 Hz (T = 20 ms)
+# Pulse: 0.5 ms (0°) → 2.5 ms (max_angle)
+# Duty % = pulse_ms / 20 ms * 100  →  0.5ms = 2.5%,  2.5ms = 12.5%
 
+
+class RDX3225:
+    def __init__(self, pinNum, max_angle=270):
+        self.pinNum    = pinNum
+        self.max_angle = max_angle
+        GPIO.setup(self.pinNum, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pinNum, 50)
+        self.pwm.start(0)
+        self.setAngle(0)
+
+    def _duty(self, angle):
+        return 2.5 + (angle / self.max_angle) * 10.0
+
+    def setAngle(self, angle):
+        angle      = max(0, min(self.max_angle, angle))
+        self.angle = angle
+        self.pwm.ChangeDutyCycle(self._duty(angle))
+        time.sleep(0.3)
+        self.pwm.ChangeDutyCycle(0)  # stop signal to reduce jitter
+
+
+class Mod_RDX3225:
+    def __init__(self, pinNum):
+        self.servo = RDX3225(pinNum)
+
+    def onRun(self):
+        self.servo.setAngle(0)
+        time.sleep(1)
+        self.servo.setAngle(270)
+        time.sleep(1)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import gpio_init
 import drivers.SG92R
 import drivers.MG995
+import drivers.RDX3225
 
 
 
@@ -10,11 +11,11 @@ import drivers.MG995
 
 ## welcomming message
 print("welcome to vlaArm mainloop")
-graber = drivers.MG995.Mod_MG995(18)
+arm = drivers.RDX3225.Mod_RDX3225(18)
 
 ##mainloop
 cntLoop = 0
 while True:
     print("________loop" + str(cntLoop) + "_______")
-    graber.onRun()
+    arm.onRun()
     cntLoop += 1
